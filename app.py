4ab0322b475c0c0c353956f7630d4872b913acf1
Paul Zuradzki <paulzuradzki@gmail.com>
2021-08-31 22:37:25 +0200

    fixed broken style/CSS rendering

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import dash
 import flask
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
+# for styling / CSS
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+    'href': 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+    'rel': 'stylesheet',
+    'integrity': 'sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO',
+    'crossorigin': 'anonymous'
+    }]
 
 server = flask.Flask(__name__) # define flask app.server
+
 app = dash.Dash(__name__, 
                 suppress_callback_exceptions=True, 
-                external_stylesheets=external_stylesheets, 
-                server=server)
-# server = app.server
+                external_stylesheets=external_stylesheets, server=server)
